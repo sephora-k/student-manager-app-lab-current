f542d34d0f595e2653324474bd202fb150ad35ff
danielcregg <daniel.cregg@gmail.com>
2019-10-21 15:48:21 +0100

    Updated Menu class

--- src/ie/gmit/studentmanager/Main.java
+++ src/ie/gmit/studentmanager/Main.java
@@ -4,21 +4,9 @@ public class Main {
     
     public static void main(String[] args){
         // Create a new menu object
- //   	Menu menuObject1 = new Menu();
+    	Menu menuObject1 = new Menu();
     	// Run the start method of the Menu object 
- //   	menuObject1.display();
-    	
-    	// Test StudentManagerObject Class
-    	StudentManager smObject1 = new StudentManager();
-    	smObject1.loadStudentFile(".\\resources\\students.csv");
-    	//System.out.println(    smObject1.findTotalStudents()   );
- //   	smObject1.findStudentById("G00654323");
-    	System.out.println(   smObject1.findTotalStudents()    );
-//    	Student student1 = new Student("G00123456");
-//    	smObject1.addStudent(student1);
-    	Student student = smObject1.findStudentById("G00654324");
-    	System.out.println(student.getFirstName());
-//    	System.out.println(student);
+    	menuObject1.display();
     	
     }
 }
--- src/ie/gmit/studentmanager/Menu.java
+++ src/ie/gmit/studentmanager/Menu.java
@@ -1,11 +1,13 @@
 package ie.gmit.studentmanager;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-	private Scanner userInput;
-	private StudentManager studentManagerObject;
-	private boolean keepRunning;
+	// Instance Variables
+	private Scanner userInput = null;
+	private StudentManager studentManagerObject = null;
+	private boolean keepRunning = false;
 	
 	// Constructor Method
 	public Menu() {
@@ -21,10 +23,10 @@ public class Menu {
 			showOptions();
 
 			// Save user Menu option selection
-			int userMenuOption = userInput.nextInt();
+			int userSelectedOption = userInput.nextInt();
 
 			// Execute option
-			selectOption(userMenuOption);
+			selectOption(userSelectedOption);
 		}
 	}
 
@@ -36,47 +38,86 @@ public class Menu {
 		System.out.println("(2) Add a Student.");
 		System.out.println("(3) Delete a Student.");
 		System.out.println("(4) Search for a Student by ID.");
-		System.out.println("(5) Search for Students by Firt Name.");
+		System.out.println("(5) Search for Students by First Name.");
 		System.out.println("(6) Show total Number of Students.");
 		System.out.println("(7) Save DB.");
 		System.out.println("(8) Quit.");
-		System.out.println("Select an option [1-6]>");
+		System.out.println("Select an option [1-8]>");
 	}
 
 	private void selectOption(int userSelection) {
 		if (userSelection == 1) { // Load Students DB
 			System.out.println("User Selected 1");
+		} else if (userSelection == 2) {  // Add Student
+			addStudent();
+		} else if (userSelection == 3) {  // Delete Student
+			deleteStudent();
+		} else if (userSelection == 4) {  // Search Student by ID
+			searchByID();
+		} else if (userSelection == 5) {  // Search Students by Name
+			searchByName();
+		} else if (userSelection == 6) {  // Search Students by Name
+			System.out.println("Total number of students = " +
+					studentManagerObject.findTotalStudents());
+		} else if (userSelection == 8) {
 			keepRunning = false;
-		} else if (userSelection == 2) { // Add Student
-			System.out.println("Enter Student ID>");
-			String studentId = userInput.next();
-			Student newStudentObject = new Student(studentId);
-			
-			boolean result = studentManagerObject.addStudent(newStudentObject);
-
-			if (result) {
-				System.out.println("Student " + studentId + " has been added successfully.");
-			} else {
-				
-				System.out.println("ERROR: Student " + studentId +"  was not added!");
-			}
+			System.out.println("Goodbye!");
 		}
+			
 	}
 
 	private void addStudent() {
+		System.out.println("Enter Student ID>");
+		String studentId = userInput.next();
+		System.out.println("Enter Student Name>");
+		String studentName = userInput.next();
+		System.out.println("Enter Student Surname>");
+		String studentSurname = userInput.next();
+
+		Student newStudentObject = new Student(studentId, studentName, studentSurname);
+		
+		boolean result = studentManagerObject.addStudent(newStudentObject);
+
+		if (result) {
+			System.out.println("Student " + studentId + " has been added successfully.");
+		} else {
+			System.out.println("ERROR: Student " + studentId +"  was not added!");
+		}
 	}
 
 	private void deleteStudent() {
+		System.out.println("Enter Student ID>");
+		String studentId = userInput.next();
+		boolean result = studentManagerObject.deleteStudentById(studentId);
+		if (result) {
+			System.out.println("Student " + studentId + " has been deleted successfully.");
+		} else {
+			System.out.println("ERROR: Student " + studentId +"  was not deleted!");
+		}
 	}
 
 	private void searchByID() {
+		System.out.println("Enter Student ID>");
+		String studentId = userInput.next();
+		Student student1 = studentManagerObject.findStudentById(studentId);
+		if (student1 == null) {
+			System.out.println("Student NOT found!");
+		} else {
+			System.out.println("Student " + student1.getStudentId() + " found!");
+		}	
 	}
 
 	private void searchByName() {
-	}
-
-	private void findTotal() {
-		System.out.println("Total number of students = " + studentManagerObject.findTotalStudents());
+		System.out.println("Enter Student Name>");
+		String studentName = userInput.next();
+		List<Student> students = studentManagerObject.getStudentsByFirstName(studentName);
+		if (students == null) {
+			System.out.println("No studnet with that first name found!");
+		} else {
+			for (Student student : students) {
+				System.out.println(     student.getStudentId()    );
+			}
+		}
 	}
 
 }
